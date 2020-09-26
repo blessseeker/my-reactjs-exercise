8fb962c977470bff9b73eb9846dcda6580742c9b
Kamaludin Khoir <khoirkamaludin@gmail.com>
2020-09-26 12:31:52 +0700

    mengambil data list user dari pengguna

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import UsersList from "../components/UsersList";
 
 const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "Kamaludin Khoir",
-      image:
-        "https://cdn.idntimes.com/content-images/avatar/kamaludin-khoir_200x200.jpg?v=80782c83ffa0f09ef565e33687c7ed8b",
-      places: 1,
-    },
-    {
-      id: "u2",
-      name: "Usi Supinar",
-      image:
-        "https://pbs.twimg.com/profile_images/852513364550877184/922tr-_V.jpg",
-      places: 2,
-    },
-  ];
-  return <UsersList items={USERS} />;
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch("http://localhost:5000/api/users/");
+        const responseData = await response.json();
+
+        setLoadedUsers(responseData.users);
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+  );
 };
 
 export default Users;
