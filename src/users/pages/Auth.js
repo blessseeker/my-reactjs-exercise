09c223e9c89b4ed053aa97b032d53e4769e70706
Kamaludin Khoir <khoirkamaludin@gmail.com>
2020-09-26 11:32:45 +0700

    menambahkan error modal dan loading spinner

--- src/users/pages/Auth.js
+++ src/users/pages/Auth.js
@@ -2,6 +2,8 @@ import React, { useState, useContext } from "react";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UIElements/Card";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
@@ -15,6 +17,8 @@ import "./Auth.css";
 const Auth = () => {
   const auth = useContext(AuthContext);
   const [isLoginMode, setIsLoginMode] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const [formState, inputHandler, setFormData] = useForm({
     email: {
@@ -33,7 +37,8 @@ const Auth = () => {
     if (isLoginMode) {
     } else {
       try {
-        const response = await fetch("http://localhost:5000/api/signup", {
+        setIsLoading(true);
+        const response = await fetch("http://localhost:5000/api/users/signup", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -46,13 +51,18 @@ const Auth = () => {
         });
 
         const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
         console.log(responseData);
+        setIsLoading(false);
+        auth.login();
       } catch (err) {
         console.log(err);
+        setIsLoading(false);
+        setError(err.message || "Terjadi kesalahan");
       }
     }
-
-    auth.login();
   };
 
   const switchModeHandler = () => {
@@ -79,47 +89,55 @@ const Auth = () => {
     setIsLoginMode((prevMode) => !prevMode);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    <Card className="authentication">
-      <h2>Silakan melakukan login</h2>
-      <form onSubmit={submitHandler}>
-        {!isLoginMode && (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      <Card className="authentication">
+        {isLoading && <LoadingSpinner asOverlay />}
+        <h2>Silakan melakukan login</h2>
+        <form onSubmit={submitHandler}>
+          {!isLoginMode && (
+            <Input
+              id="nama"
+              element="input"
+              type="text"
+              label="Nama Lengkap"
+              validators={[VALIDATOR_REQUIRE()]}
+              errorText="Harap isi dengan nama lengkap Anda"
+              onInput={inputHandler}
+            />
+          )}
           <Input
-            id="nama"
+            id="email"
             element="input"
-            type="text"
-            label="Nama Lengkap"
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="Harap isi dengan nama lengkap Anda"
+            type="email"
+            label="E-mail"
+            validators={[VALIDATOR_EMAIL()]}
+            errorText="Masukkan email yang valid"
             onInput={inputHandler}
           />
-        )}
-        <Input
-          id="email"
-          element="input"
-          type="email"
-          label="E-mail"
-          validators={[VALIDATOR_EMAIL()]}
-          errorText="Masukkan email yang valid"
-          onInput={inputHandler}
-        />
-        <Input
-          id="password"
-          element="input"
-          type="password"
-          label="Password"
-          validators={[VALIDATOR_MINLENGTH(6)]}
-          errorText="Password harus terdiri minimal 6 karakter"
-          onInput={inputHandler}
-        />
-        <Button type="submit" disabled={!formState.isValid}>
-          {isLoginMode ? "LOGIN" : "DAFTAR"}
+          <Input
+            id="password"
+            element="input"
+            type="password"
+            label="Password"
+            validators={[VALIDATOR_MINLENGTH(6)]}
+            errorText="Password harus terdiri minimal 6 karakter"
+            onInput={inputHandler}
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            {isLoginMode ? "LOGIN" : "DAFTAR"}
+          </Button>
+        </form>
+        <Button inverse onClick={switchModeHandler}>
+          {isLoginMode ? "SAYA BELUM DAFTAR" : "SAYA SUDAH PUNYA AKUN"}
         </Button>
-      </form>
-      <Button inverse onClick={switchModeHandler}>
-        {isLoginMode ? "SAYA BELUM DAFTAR" : "SAYA SUDAH PUNYA AKUN"}
-      </Button>
-    </Card>
+      </Card>
+    </React.Fragment>
   );
 };
 
