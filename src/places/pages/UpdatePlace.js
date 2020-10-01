c12a3c6cc69f6769fd7d97f742ebdb1a9737a7e5
Kamaludin Khoir <khoirkamaludin@gmail.com>
2020-10-01 22:21:37 +0700

    update data di database melalui backend

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,60 +1,34 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState, useContext } from "react";
+import { useParams, useHistory } from "react-router-dom";
 
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UIElements/Card";
-import { useForm } from "../../shared/hooks/form";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH,
 } from "../../shared/util/validators";
+import { useForm } from "../../shared/hooks/form";
+import { useHttpClient } from "../../shared/hooks/http";
+import { AuthContext } from "../../shared/context/auth-context";
 import "./PlaceForm.css";
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    judul: "Batu Kuda",
-    deskripsi: "Wisata Pinus",
-    imageUrl:
-      "https://cdn.ayobandung.com/images-bandung/post/articles/2018/01/06/27192/whatsapp_image_2018-01-06_at_20.05.17.jpeg",
-    alamat: "Cibiru Wetan, Cileunyi, Bandung, Jawa Barat 40625",
-    location: {
-      lat: -6.8936106,
-      lng: 107.7425512,
-    },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    judul: "Empire State Building",
-    deskripsi: "One of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    alamat: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    creator: "u2",
-  },
-];
-
 const UpdatePlace = () => {
-  const [isLoading, setIsLoading] = useState(true);
+  const auth = useContext(AuthContext);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedPlace, setLoadedPlace] = useState();
   const placeId = useParams().placeId;
+  const history = useHistory();
 
   const [formState, inputHandler, setFormData] = useForm(
     {
-      judul: {
+      title: {
         value: "",
         isValid: false,
       },
-      deskripsi: {
-        value: "",
-        isValid: false,
-      },
-      alamat: {
+      description: {
         value: "",
         isValid: false,
       },
@@ -62,91 +36,99 @@ const UpdatePlace = () => {
     false
   );
 
-  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
-
   useEffect(() => {
-    if (identifiedPlace) {
-      setFormData(
-        {
-          judul: {
-            value: identifiedPlace.judul,
-            isValid: true,
+    const fetchPlace = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/places/${placeId}`
+        );
+        setLoadedPlace(responseData.place);
+        setFormData(
+          {
+            title: {
+              value: responseData.place.title,
+              isValid: true,
+            },
+            description: {
+              value: responseData.place.description,
+              isValid: true,
+            },
           },
-          deskripsi: {
-            value: identifiedPlace.deskripsi,
-            isValid: true,
-          },
-          alamat: {
-            value: identifiedPlace.alamat,
-            isValid: true,
-          },
-        },
-        true
-      );
-    }
-    setIsLoading(false);
-  }, [setFormData, identifiedPlace]);
+          true
+        );
+      } catch (err) {}
+    };
+    fetchPlace();
+  }, [sendRequest, placeId, setFormData]);
 
-  const placeSubmitHandler = (event) => {
+  const placeUpdateSubmitHandler = async (event) => {
     event.preventDefault();
-    console.log(formState.inputs);
+    try {
+      await sendRequest(
+        `http://localhost:5000/api/places/${placeId}`,
+        "PATCH",
+        JSON.stringify({
+          title: formState.inputs.title.value,
+          description: formState.inputs.description.value,
+        }),
+        {
+          "Content-Type": "application/json",
+        }
+      );
+      history.push("/" + auth.userId + "/places");
+    } catch (err) {}
   };
 
-  if (!identifiedPlace) {
+  if (isLoading) {
+    return (
+      <div className="center">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (!loadedPlace && !error) {
     return (
       <div className="center">
         <Card>
-          <h2>Tempat tidak ditemukan</h2>
+          <h2>Could not find place!</h2>
         </Card>
       </div>
     );
   }
 
-  if (isLoading) {
-    return (
-      <div className="center">
-        <h2>Sedang memuat</h2>
-      </div>
-    );
-  }
-
   return (
-    <form className="place-form" onSubmit={placeSubmitHandler}>
-      <Input
-        id="judul"
-        type="text"
-        label="Judul"
-        element="input"
-        validators={[VALIDATOR_REQUIRE()]}
-        onInput={inputHandler}
-        errorText="Masukkan judul"
-        value={formState.inputs.judul.value}
-        valid={formState.inputs.judul.isValid}
-      />
-      <Input
-        id="deskripsi"
-        label="Deskripsi"
-        element="textarea"
-        validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Masukkan minimal lima karakter"
-        onInput={inputHandler}
-        value={formState.inputs.deskripsi.value}
-        valid={formState.inputs.deskripsi.isValid}
-      />
-      <Input
-        id="alamat"
-        label="Alamat"
-        element="textarea"
-        validators={[VALIDATOR_REQUIRE()]}
-        onInput={inputHandler}
-        errorText="Masukkan alamat"
-        value={formState.inputs.alamat.value}
-        valid={formState.inputs.alamat.isValid}
-      />
-      <Button type="submit" disabled={!formState.isValid}>
-        Simpan Perubahan
-      </Button>
-    </form>
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {!isLoading && loadedPlace && (
+        <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+          <Input
+            id="title"
+            element="input"
+            type="text"
+            label="Title"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please enter a valid title."
+            onInput={inputHandler}
+            initialValue={loadedPlace.title}
+            initialValid={true}
+          />
+          <Input
+            id="description"
+            element="textarea"
+            label="Description"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid description (min. 5 characters)."
+            onInput={inputHandler}
+            initialValue={loadedPlace.description}
+            initialValid={true}
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            UPDATE PLACE
+          </Button>
+        </form>
+      )}
+    </React.Fragment>
   );
 };
 
